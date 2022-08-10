305565a14b77f67f0613969cfd51a12292b0716e
waqas ahmad <waqaskanju@gmail.com>
2022-08-10 23:07:29 +0500

    add all queries

--- data.sql
+++ data.sql
@@ -3,3 +3,18 @@
 /* INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie'); */
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (1,'Agumon', '02-03-2020', 0, 'yes', 10.23);
+
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (2,'Gabumon', '11-15-2018', 2, 'yes', 8);
+
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (3,'Pikachu', '01-07-2021', 1, 'No', 15.04);
+
+INSERT INTO public.animals(
+	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+	VALUES (4,'Devimon', '05-12-2017', 5, 'Yes', 11);
--- schema.sql
+++ schema.sql
@@ -1,6 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
-/* CREATE TABLE animals (
-    name varchar(100)
+ CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+CREATE TABLE public.animals
+(
+    id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric NOT NULL,
+    PRIMARY KEY (id)
 );
- */
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
