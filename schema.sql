1ed4c454197681b43ae30d14916b454c1f749893
waqas ahmad <waqaskanju@gmail.com>
2022-08-13 02:04:26 +0500

    combine queries

--- schema.sql
+++ schema.sql
@@ -24,3 +24,43 @@ ALTER TABLE IF EXISTS public.animals
 
     ALTER TABLE IF EXISTS public.animals
     ADD COLUMN species character varying(30) COLLATE pg_catalog."default";
+
+    /* Project 3 query multiple tables */
+
+ۜأ/* Create Owners table */
+
+    CREATE TABLE public.owners
+(
+    id serial NOT NULL,
+    full_name character varying(30) NOT NULL,
+    age integer NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+    ALTER TABLE IF EXISTS public.owners
+    OWNER to postgres;
+
+/* Create Species Table  */
+    CREATE TABLE public.species
+(
+    id serial NOT NULL,
+    name character varying(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+    ALTER TABLE IF EXISTS public.species
+    OWNER to postgres;
+
+
+    /* Remove Species column  from animale table*/
+
+     ALTER TABLE animals DROP COLUMN species;
+
+
+     /* Add Species_id column */
+
+    ALTER TABLE animals add COLUMN species_id INTEGER REFERENCES species (id) 
+    
+    /* Add owners_id column */
+  ALTER TABLE animals add COLUMN owner_id INTEGER REFERENCES owners (id) 
